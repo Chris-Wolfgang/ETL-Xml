25734ccee8f89d069b1e041a43135bbf62bf9670
github-action-benchmark <github@users.noreply.github.com>
2026-08-16 06:09:18 +0000

    add Stryker mutation score (customBiggerIsBetter) benchmark result for 653a677784fea378db6a06dfc7a65594c3e95c74

---
+++ dev/stryker/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1786860557297,
+  "repoUrl": "https://github.com/Chris-Wolfgang/ETL-Xml",
+  "entries": {
+    "Stryker mutation score": [
+      {
+        "commit": {
+          "author": {
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang",
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "653a677784fea378db6a06dfc7a65594c3e95c74",
+          "message": "Merge pull request #269 from Chris-Wolfgang/chore/baseline-0.8.0\n\nchore(release): advance PackageValidation baseline to 0.8.0",
+          "timestamp": "2026-08-14T19:18:25Z",
+          "url": "https://github.com/Chris-Wolfgang/ETL-Xml/commit/653a677784fea378db6a06dfc7a65594c3e95c74"
+        },
+        "date": 1786860554762,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Mutation score",
+            "value": 71.55,
+            "unit": "%"
+          }
+        ]
+      }
+    ]
+  }
+}
