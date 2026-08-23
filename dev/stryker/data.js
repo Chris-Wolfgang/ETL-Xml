f4f9e76081d1224938d540a4b951e0a47295aa1b
github-action-benchmark <github@users.noreply.github.com>
2026-08-23 06:09:06 +0000

    add Stryker mutation score (customBiggerIsBetter) benchmark result for 60fae5bae3b685a392dbbb551c7ce40a962a3e79

--- dev/stryker/data.js
+++ dev/stryker/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786860557297,
+  "lastUpdate": 1787465346066,
   "repoUrl": "https://github.com/Chris-Wolfgang/ETL-Xml",
   "entries": {
     "Stryker mutation score": [
@@ -21,6 +21,33 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Chris-Wolfgang/ETL-Xml/commit/653a677784fea378db6a06dfc7a65594c3e95c74"
         },
         "date": 1786860554762,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Mutation score",
+            "value": 71.55,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang",
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "60fae5bae3b685a392dbbb551c7ce40a962a3e79",
+          "message": "Merge pull request #278 from Chris-Wolfgang/chore/baseline-0.8.1\n\nchore(release): advance PackageValidation baseline to 0.8.1",
+          "timestamp": "2026-08-21T18:17:40Z",
+          "url": "https://github.com/Chris-Wolfgang/ETL-Xml/commit/60fae5bae3b685a392dbbb551c7ce40a962a3e79"
+        },
+        "date": 1787465342890,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
