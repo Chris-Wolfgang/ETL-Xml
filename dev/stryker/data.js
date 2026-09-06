6daf9666166634ef8375453e786aa099f66d3484
github-action-benchmark <github@users.noreply.github.com>
2026-09-06 06:06:02 +0000

    add Stryker mutation score (customBiggerIsBetter) benchmark result for a55cd59e4f30de51fcbcd250d917b4633d7e6e24

--- dev/stryker/data.js
+++ dev/stryker/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788070038114,
+  "lastUpdate": 1788674761657,
   "repoUrl": "https://github.com/Chris-Wolfgang/ETL-Xml",
   "entries": {
     "Stryker mutation score": [
@@ -75,6 +75,33 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Chris-Wolfgang/ETL-Xml/commit/60fae5bae3b685a392dbbb551c7ce40a962a3e79"
         },
         "date": 1788070035456,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Mutation score",
+            "value": 71.55,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang",
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "a55cd59e4f30de51fcbcd250d917b4633d7e6e24",
+          "message": "Merge pull request #287 from Chris-Wolfgang/dependabot/github_actions/github-actions-2390256866\n\nchore(deps): bump the github-actions group across 1 directory with 4 updates",
+          "timestamp": "2026-09-05T15:37:20Z",
+          "url": "https://github.com/Chris-Wolfgang/ETL-Xml/commit/a55cd59e4f30de51fcbcd250d917b4633d7e6e24"
+        },
+        "date": 1788674759225,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
